28fec79754b17282d2886fb6ee4517f81d7dd1e6
YasseenR <114093367+YasseenR@users.noreply.github.com>
2025-08-29 10:16:39 -0400

    perfect square

--- activity.py
+++ activity.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def randNums():
 
@@ -7,13 +8,56 @@ def randNums():
     for x in range(100):
         defaultArray.append(x)
 
-    for y in range(20):
-        dafaultArray[y] = random.randint(1, 100)
+    for y in range(30):
+        defaultArray[y] = random.randint(1, 100)
     
     for z in range(len(defaultArray)):
-        if (defaultArray % 2) == 0:
-            print("Even Number Found: {z}")
+        if (defaultArray[z] % 2) == 0:
+            print(f"Even Number Found: {defaultArray[z]}")
         else:
-            print("Odd Number Found: {z}")
-    
+            print(f"Odd Number Found: {defaultArray[z]}")
 
+    total = 0
+    for num in range(len(defaultArray)):
+        total += defaultArray[num]
+    print(f"Total Sum: {total}")
+
+    max = 0
+
+    for num in range(len(defaultArray)):
+        if num > max and isPrime(num):
+            max = num
+
+    print(max)
+
+    for x in range(len(defaultArray)):
+        if x == 0:
+            return True
+        sqrtNum = math.sqrt(x)
+
+        if sqrtNum.is_integer():
+            return True
+        else:
+            return False
+
+
+        
+        
+
+
+def isPrime(num):
+    if num <= 1:
+        return False
+    elif num == 2:
+        return True
+    elif num % 2 == 0:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    
+    
+    
+    return True
+
+randNums()
